e55a5af582bf0176781aeab75d8443cc9e610975
cuaviet <hoang-anh.nguyen@hotmail.com>
2016-01-29 15:37:09 +0100

    Demo specflow

---
+++ HelloWorld/HelloWorld.Specs/FeatureSteps/FizzBuzzFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace HelloWorld.Specs.FeatureSteps
+{
+    [Binding]
+    public class FizzBuzzFeatureSteps
+    {
+        private FizzBuzz _fizzBuzz = new FizzBuzz();
+
+        [Given(@"I have entered (.*)")]
+        public void GivenIHaveEntered(int number)
+        {
+            ScenarioContext.Current.Add("Input",number);
+        }
+        
+        [When(@"I call FizzBuzzTranslator")]
+        public void WhenICallFizzBuzzTranslator()
+        {
+            int number = Convert.ToInt32(ScenarioContext.Current["Input"]);
+            string output = _fizzBuzz.GetFizzBuzz(number);
+            ScenarioContext.Current.Add("Output",output);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(string expected)
+        {
+            string output = ScenarioContext.Current["Output"].ToString();
+            Assert.AreEqual(expected,output);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.Specs/FizzBuzzFeature.feature
@@ -0,0 +1,8 @@
+﻿Feature: FizzBuzzFeature
+		I wanna to check output of FizzBuzz feature
+
+@FizzBuzz
+Scenario: Enter zero
+	Given I have entered 0
+	When I call FizzBuzzTranslator
+	Then the result should be "0"
