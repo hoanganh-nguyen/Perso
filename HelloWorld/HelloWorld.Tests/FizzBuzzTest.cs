d65cf497f9a59b70bee934a9d67be46a8668105d
cuaviet <hoang-anh.nguyen@hotmail.com>
2016-01-29 15:21:12 +0100

    Add HelloWorld

---
+++ HelloWorld/HelloWorld.Tests/FizzBuzzTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        FizzBuzz _fizzBuzz;
+        [SetUp]
+        public void Init()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        [Test]
+        public void TestGetFizzBuzz()
+        {
+            var tmp = _fizzBuzz.GetFizzBuzz(3);
+            Assert.AreEqual("Fizz", tmp);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class FizzBuzz
+    {
+        public string GetFizzBuzz(int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (i % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (i % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return i.ToString();
+        }
+    }
+}
