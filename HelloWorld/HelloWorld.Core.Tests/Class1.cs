83273a35bfe5242ddffbc586da078cc0627cf766
CuaFrance <souslevent@gmail.com>
2016-02-03 15:51:35 +0100

    Continuous delivery

--- HelloWorld/HelloWorld.Core.Tests/Class1.cs
+++ HelloWorld/HelloWorld.Core.Tests/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace HelloWorld.Core.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void DoTest()
+        {
+            var ex = 1;
+
+        }
     }
 }
